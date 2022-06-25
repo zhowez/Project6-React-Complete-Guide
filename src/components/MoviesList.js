30b49124bbf9f1f06449e497406f2b72f6ad0b1a
Zach Howes <zhowes14@icloud.com>
2022-06-24 23:05:24 -0400

    added get request

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import Movie from './Movie';
-import classes from './MoviesList.module.css';
+import Movie from "./Movie";
+import classes from "./MoviesList.module.css";
 
-const MovieList = (props) => {
+const MoviesList = (props) => {
   return (
-    <ul className={classes['movies-list']}>
+    <ul className={classes["movies-list"]}>
       {props.movies.map((movie) => (
         <Movie
           key={movie.id}
@@ -18,4 +18,4 @@ const MovieList = (props) => {
   );
 };
 
-export default MovieList;
+export default MoviesList;
